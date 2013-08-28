16dbe97142e4ca66b1c91253ebbcd1c47ddabc58
machty <machty@gmail.com>
2013-08-28 17:21:49 -0400

    Initial bean-player component

--- app/components/bean-player.js
+++ app/components/bean-player.js
@@ -1,4 +1,24 @@
 var Player = Ember.Component.extend({
+
+  isPlaying: false,
+
+  play: function() {
+    this.set('isPlaying', true);
+  },
+
+  pause: function() {
+    this.set('isPlaying', false);
+  },
+
+  progressChanged: function() {
+
+    var oldProgress = this.get('progress');
+
+    // TODO: things
+  }.observes('progress'),
+
+  count: 12345
+
 });
 
 export default Player;
