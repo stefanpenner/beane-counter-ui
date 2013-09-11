22f0af8408a959459bad0a3534787e1eed8a1530
machty <machty@gmail.com>
2013-09-11 15:47:46 -0400

    Unwatch/rewatch bean team standings when changing seasons

--- app/components/bean-team-standing.js
+++ app/components/bean-team-standing.js
@@ -1,16 +1,38 @@
+import demux from 'appkit/ziggrid/demux';
+
 var TeamStanding = Ember.Component.extend({
+  _applicationController: Ember.computed(function(){
+    return this.container.lookup('controller:application');
+  }),
+
+  season: Ember.computed.alias('_applicationController.season'),
   team: null,
+  handle: null,
   watcher: Ember.computed(function() {
     return this.container.lookup('watcher:main');
   }),
+
   winLoss: function() {
     var watcher = this.get('watcher');
+    var code = this.get('team.code');
+    var season = this.get('season');
 
-    // TODO: unwatch (although this demo wont need it)
-    return watcher.watch('WinLoss', 'WinLoss', {
-      team: this.get('team.code')
+    var handle = this.get('handle');
+
+    if (handle) {
+      // unsubscribe;
+      watcher.unwatch(handle);
+    }
+
+    var model = watcher.watch('WinLoss', 'WinLoss', {
+      team: code,
+      season: season
     });
-  }.property(),
+
+    this.set('handle', demux.lastId);
+
+    return model;
+  }.property('team.code', 'season'),
   wins: Ember.computed.alias('winLoss.wins'),
   losses: Ember.computed.alias('winLoss.losses')
 });
