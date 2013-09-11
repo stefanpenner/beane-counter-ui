e5e6a67925695c816f4121ccff7f1faeb3f4ab90
machty <machty@gmail.com>
2013-09-11 18:03:00 -0400

    Date text for progress slider comes from GameDate

--- app/controllers/quadrant.js
+++ app/controllers/quadrant.js
@@ -1,20 +1,29 @@
 import { percentageOfData } from 'appkit/utils/percentage_of_data';
 
+var get = Ember.get;
+
 var QuadrantController = Ember.Controller.extend({
   needs: ['filter'],
   filter: Ember.computed.alias('controllers.filter.selectedFilter'),
 
   progress: function() {
-    var gameDate = this.get('gameDates.lastObject');
+    var gameDate = this.get('currentDate');
 
     if (!gameDate) {
       return 0;
     }
 
     return percentageOfData(gameDate.day, parseInt(gameDate.season, 10));
-  }.property('gameDates.lastObject'),
+  }.property('currentDate'),
 
-  currentDateText: "September 12, 2012"
+  currentDate: Ember.computed.alias('gameDates.lastObject'),
+  //currentDate: { season: "2009", day: "180" },
+
+  currentDateText: function() {
+    var date = this.get('currentDate');
+    if (!date) { return; }
+    return moment("" + get(date, 'season')).day(parseInt(get(date, 'day'), 10) + 1).format("MMMM D, YYYY");
+  }.property('currentDate')
 });
 
 export default QuadrantController;
